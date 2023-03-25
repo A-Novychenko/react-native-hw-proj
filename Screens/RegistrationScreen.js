b92596ecdfd716a168c0f3a2bbe5f041c6151060
Aleksandr Novychenko <A.Novychenko@outlook.com>
2023-03-26 01:28:39 +0200

    upd

--- Screens/RegistrationScreen.js
+++ Screens/RegistrationScreen.js
@@ -1,11 +1,21 @@
 import React, {useState} from "react";
-import {StyleSheet, View, TextInput, Image, Button, Text} from "react-native";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Image,
+  Button,
+  Text,
+  TouchableOpacity,
+} from "react-native";
 export const RegistrationScreen = () => {
   return (
     <View style={styles.formWrap}>
       <View style={styles.avatar}>
         <Image style={styles.avatarImg} />
-        <Button title="+" />
+        <TouchableOpacity style={styles.avatarAdd} activeOpacity={0.8}>
+          <Text style={styles.avatarText}>+</Text>
+        </TouchableOpacity>
       </View>
       <Text style={styles.title}>Регистрация</Text>
       <View style={styles.inputBox}>
@@ -31,14 +41,39 @@ const styles = StyleSheet.create({
 
   avatar: {
     position: "absolute",
-    top: 0,
-    left: 0,
-    // width: 120,
-    // height: 120,
+    top: "50%",
+    left: "50%",
+    transform: [{translateX: -55}, {translateY: -210}],
+
+    //   !!!!!!!!!!!!!как задать в процентах или как это правильно сделать в реакт???
   },
   avatarImg: {
     width: 120,
     height: 120,
+    backgroundColor: "red",
+  },
+
+  avatarAdd: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    transform: [{translateX: 105}, {translateY: 80}],
+
+    //   !!!!!!!!!!!!!как задать в процентах или как это правильно сделать в реакт???
+
+    width: 25,
+    height: 25,
+    border: 1,
+    borderColor: "#FF6C00",
+    backgroundColor: "#fff",
+    borderRadius: 12.5,
+  },
+
+  avatarText: {
+    marginRight: "auto",
+    marginLeft: "auto",
+    fontSize: 20,
+    color: "#FF6C00",
   },
 
   title: {
